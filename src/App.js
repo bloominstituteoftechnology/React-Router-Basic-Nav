2b8f1bfddd4b0d47f4c1076dd07c3166a601ae7e
Emily Sperry <sperrye@gmail.com>
2017-08-02 12:14:17 -0700

    mini sprint solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
+import { Home, About, Contact } from './components';
+import { Route } from 'react-router-dom';
 
-const App = () => <Navigation />;
+const App = () => (
+  <div>
+    <Navigation />
+    <Route path="/" component={ Home }/>
+    <Route path="/about" component={ About }/>
+    <Route path="/contact" component={ Contact }/>
+  </div>
+);
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
