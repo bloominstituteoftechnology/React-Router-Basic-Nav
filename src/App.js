e3b2c860d3271e06ad67a01c8e9b999471868bd9
Ely Alamillo <el0013@gmail.com>
2017-08-02 19:19:08 -0500

    finished example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
 
 const App = () => (
   <div>
-    <Navigation /> 
+    <Navigation />
+    <Route exact path='/' component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
 )
 
