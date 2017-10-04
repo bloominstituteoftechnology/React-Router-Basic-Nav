31a5140304d44928fcdb494b4f6869551a484688
Samer Dabra <samer.dabra@googlemail.com>
2017-10-04 23:55:58 +0100

    MiniSprit Complete

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 const Navigation = () => {
   return (
     <div>
       <Nav className="App">
-        <h1>React Router Mini</h1>
         <NavItem>
-          <NavLink href="#">Home</NavLink>
+          <Link to="/" className="Nav-link">Home</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">About</NavLink>
+          <Link to="/about" className="Nav-link">About</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Contact</NavLink>
+          <Link to="/contact" className="Nav-link">Contact</Link>
         </NavItem>
       </Nav>
+      <h1>React Router Mini</h1>
     </div>
   );
 };
