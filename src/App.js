15eacd79e91579ede0bcf99ce53e5aa54c3619b9
Justin Borek <justinborek@gmail.com>
2017-11-06 13:05:47 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
 
 const App = () => (
   <div>
-    <Navigation /> 
+    <Navigation />
+    <Route exact={true} path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 )
 
