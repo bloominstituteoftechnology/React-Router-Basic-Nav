70a6713b10706d93b386005759ad1aab5b329256
Samuel Cha <sunchipssam@gmail.com>
2018-01-16 12:19:55 -0800

    got half dynamic working and started implementing Apple website styling, which removes the need for dynamic styling. can still test dynamic styling by going to /products and clicking on books

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import App from './App';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
 // import registerServiceWorker from './registerServiceWorker';
 // import 'bootstrap/dist/css/bootstrap.css';
 
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 
-/*
 function Home(props) {
 	return (
 		<div className="View">
@@ -30,49 +29,65 @@ function About(props) {
 }
 
 function Products(props) {
+	console.log('products props')
+	console.log(props);
 	return (
 		<div className="View">
-			<h1>Products Comp</h1>
-
-			<ul>
+			<ul className="NavHeader">
 				<li className="NavLink">
-					<NavLink exact to="/products" className="NavLink--title" activeClassName="NavLink--selected">
-							Home
-					</NavLink>
-				</li>
-				<li className="NavLink">
-					<NavLink exact to="/products/movies" className="NavLink--title" activeClassName="NavLink--selected">
+					<NavLink exact to="/movies" className="NavLink--title" activeClassName="NavLink--selected">
 							Movies
 					</NavLink>
 				</li>
 				<li className="NavLink">
-					<NavLink exact to="/products/music" className="NavLink--title" activeClassName="NavLink--selected">
+					<NavLink exact to="/music" className="NavLink--title" activeClassName="NavLink--selected">
 						Music
 					</NavLink>
 				</li>
 				<li className="NavLink">
-					<NavLink exact to="/products/books" className="NavLink--title" activeClassName="NavLink--selected">
+					<NavLink exact to={`${props.match.path}/books`} className="NavLink--title" activeClassName="NavLink--selected">
 						Books
 					</NavLink>
 				</li>
 			</ul>
 			<hr />
 
-			<p>This is the products comp</p>
-			<hr />
+			<p>This is the products home page</p>
 
-			<Route path="/products/movies" component={Movies} />
-			<Route path="/products/music" component={Music} />
-			<Route path="/products/books" component={Books} />
+			<Route path="/movies" component={Movies} />
+			<Route path="/music" component={Music} />
+			<Route path={`${props.match.path}/books`} component={Books} />
 		</div>
 	)
 }
 
 function Movies(props) {
+	console.log(props);
 	return (
 		<div className="View">
-			<h1>Movies Comp</h1>
-			<p>This is the movies comp</p>
+				<ul className="NavHeader">
+					<li className="NavLink">
+						<NavLink exact to="/movies" className="NavLink--title" activeClassName="NavLink--selected">
+								Movies
+						</NavLink>
+					</li>
+					<li className="NavLink">
+						<NavLink exact to="/music" className="NavLink--title" activeClassName="NavLink--selected">
+							Music
+						</NavLink>
+					</li>
+					<li className="NavLink">
+						<NavLink exact to={`${props.match.path}/books`} className="NavLink--title" activeClassName="NavLink--selected">
+							Books
+						</NavLink>
+					</li>
+			</ul>
+			<hr />
+
+			<div>
+				<h1>Movies Comp</h1>
+				<p>This is the movies comp</p>
+			</div>
 		</div>
 	)
 }
@@ -80,6 +95,25 @@ function Movies(props) {
 function Music(props) {
 	return (
 		<div className="View">
+				<ul className="NavHeader">
+					<li className="NavLink">
+						<NavLink exact to="/movies" className="NavLink--title" activeClassName="NavLink--selected">
+								Movies
+						</NavLink>
+					</li>
+					<li className="NavLink">
+						<NavLink exact to="/music" className="NavLink--title" activeClassName="NavLink--selected">
+							Music
+						</NavLink>
+					</li>
+					<li className="NavLink">
+						<NavLink exact to={`${props.match.path}/books`} className="NavLink--title" activeClassName="NavLink--selected">
+							Books
+						</NavLink>
+					</li>
+			</ul>
+			<hr />
+
 			<h1>Music Comp</h1>
 			<p>This is the music comp</p>
 		</div>
@@ -99,7 +133,7 @@ function Books(props) {
 ReactDOM.render(
 	<Router>
 		<div>
-			<ul>
+			<ul className="NavHeader">
 				<li className="NavLink">
 					<NavLink exact to="/" className="NavLink--title" activeClassName="NavLink--selected">
 							Home
@@ -116,20 +150,23 @@ ReactDOM.render(
 					</NavLink>
 				</li>
 			</ul>
+
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
 			<Route path="/products" component={Products} />
+			<Route path="/movies" component={Movies} />
+			<Route path="/music" component={Music} />
 		</div>
 	</Router>,
 	document.getElementById('root')
 );
-*/
 
+/*
 ReactDOM.render(
 	<Router>
 		<div>
 			<App />
-			
+
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
 			<Route path="/contact" component={Contact} />
@@ -137,4 +174,5 @@ ReactDOM.render(
 	</Router>,
 	document.getElementById('root')
 );
+*/
 // registerServiceWorker();
