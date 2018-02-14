00043e245bc414103c3cfefc6b29bfe3880b42fd
Charlie Sparks <Charlie@Charlies-MacBook-Pro.local>
2018-02-14 15:46:46 -0800

    completed and working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route} from "react-router-dom";
 import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
@@ -6,6 +7,9 @@ import { Home, About, Contact} from './components';
 const App = () => (
   <div>
     <Navigation /> 
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/contact" exact component={Contact} />
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+<Router>
+    <App />
+</Router>
+, document.getElementById('root'));
