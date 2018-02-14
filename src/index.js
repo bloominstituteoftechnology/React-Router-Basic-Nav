0b481d5d58323cf7f2b6f70c2dde43727602efe4
shobanavv <shobanavv@gmail.com>
2018-02-14 12:31:57 -0800

    Created Routes and basic components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
