a83029c00376d4635a741f45275e3e8d13225621
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-03-12 13:29:15 -0700

    class example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
-import Navigation from './components/Navigation';
-import { Home, About, Contact} from './components';
+
+import { Navigation, Home, About, Contact } from './components';
 
 const App = () => (
-  <div>
-    <Navigation /> 
-  </div>
+  <Router>
+    <div>
+      <Navigation /> 
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
 )
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
+import Navigation from './Navigation';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 
 export { 
+  Navigation,
   Home,
   About,
-  Contact,
+  Contact
 };
