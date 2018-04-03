4f21b4746e19aa43a29f78da83fa362ef5ff3bc4
Enjoyli Harrell <joli@Enjoylis-iMac.local>
2018-04-02 19:54:58 -0700

    Set up Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -7,13 +8,13 @@ const Navigation = () => {
       <Nav className="App">
         <h1>React Router Mini</h1>
         <NavItem>
-          <NavLink href="#">Home</NavLink>
+          <Link to="/">Home</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">About</NavLink>
+          <Link to="/about">About</Link>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Contact</NavLink>
+          <Link to="/contact">Contact</Link>
         </NavItem>
       </Nav>
     </div>
