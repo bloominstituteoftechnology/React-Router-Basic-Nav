0e0f2926d51684599cbd07354e2fb0b5f3f076fd
Bhavik Ravani <ravanibhavik@gmail.com>
2018-04-02 22:52:02 -0400

    Completed mini sprint challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact} from './components';
 
 const App = () => (
   <div>
+    <Route exact path="/" component={ Home } />
+    <Route path="/about" component={ About } />
+    <Route path="/contact" component={ Contact } />
     <Navigation /> 
   </div>
 )
