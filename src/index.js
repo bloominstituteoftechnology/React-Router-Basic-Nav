aebe6f0387c57d38d66485208d6333861ec405a7
JPEmery <clearkeyemery@gmail.com>
2018-04-02 21:40:31 -0500

    JP EMery: Mini Complete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+ReactDOM.render(<Router>
+    <App />
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    </Router>
+  , document.getElementById('root'));
 registerServiceWorker();
