d6cdf9c012e5db0b837f59a3b28aafbe52c6d649
Enjoyli Harrell <joli@Enjoylis-iMac.local>
2018-04-02 19:22:54 -0700

    Add import statement for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
