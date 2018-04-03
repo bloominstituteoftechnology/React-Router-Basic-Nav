e72312b8a019c1815a94bf8c4f1ccfa8d160d000
Enjoyli Harrell <joli@Enjoylis-iMac.local>
2018-04-02 20:00:57 -0700

    Add padding to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Nav, NavItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+const linkStyles = {
+  padding: '2rem',
+}
+
 const Navigation = () => {
   return (
     <div>
       <Nav className="App">
         <h1>React Router Mini</h1>
         <NavItem>
-          <Link to="/">Home</Link>
+          <Link style={linkStyles} to="/">Home</Link>
         </NavItem>
         <NavItem>
-          <Link to="/about">About</Link>
+          <Link style={linkStyles} to="/about">About</Link>
         </NavItem>
         <NavItem>
-          <Link to="/contact">Contact</Link>
+          <Link style={linkStyles} to="/contact">Contact</Link>
         </NavItem>
       </Nav>
     </div>
