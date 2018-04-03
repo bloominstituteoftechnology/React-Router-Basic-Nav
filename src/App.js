1e5290eac41f8ac2a6de6f7303af9d85d861c583
Stephanie Brooks <sbrooks84591@hotmail.com>
2018-04-02 22:06:23 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import { Home, About, Contact} from './components';
@@ -6,7 +7,14 @@ import { Home, About, Contact} from './components';
 const App = () => (
   <div>
     <Navigation /> 
+    <Route exact path="/" component={ Home } />
+    <Route path="/about" component={ About } />
+    <Route path="/contact" component={ Contact } />
   </div>
 )
 
 export default App;
+
+
+/* Step 6: create 3 `<Route />` setting their `path` prop equal to `/`, `/about`, `/contact` with their respective components.
+  - be sure to incluce the `exact` prop on the root component for `/` to make sure that it's rendering the exact component and not all the other components.  */
