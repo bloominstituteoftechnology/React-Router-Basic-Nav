e2c3fe940a3500ec51e764880a174a4d3cad7f5f
maribelcuales <maribelcuales@gmail.com>
2018-05-08 02:09:13 +0800

    declared path for routes and set up links in Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Link } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation />    
+    <Route exact path= "/" component={Home}></Route>
+    <Route path= "/about" component={About}></Route>
+    <Route path="/contact" component={Contact}></Route>
   </div>
 );
 
