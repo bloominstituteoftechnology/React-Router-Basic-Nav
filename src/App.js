6ca966d7efe14ce24018cfd6a6f1f8c21d4c8387
mr-burton <taylormade800@gmail.com>
2018-05-07 14:35:14 -0700

    added the navigation links

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
-// const App = () => (
-//   <div>
-//     <Navigation />
-//   </div>
-// );
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Router</h1>
-        </header>
-        <Link to="/">Home</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Contact">Contact</Link>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/About" component={About}></Route>
-        <Route path="/Contact" component={Contact}></Route>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/About" component={About} />
+    <Route path="/Contact" component={Contact}/>
+  </div>
+);
+
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,8 @@
-// import Home from './Home';
-// import About from './About';
-// import Contact from './Contact';
-// import Navigation from './Navigation';
-// import { BrowserRouter as Router } from 'react-router-dom'
-
-// export { Home, About, Contact, Navigation };
-
-
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
 
-export {
-    Home,
-    About,
-    Contact,
-    Navigation
-};
+export { Home, About, Contact, Navigation };
+
+
