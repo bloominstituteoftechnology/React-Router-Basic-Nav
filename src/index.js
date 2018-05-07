55b49a2ca1855ca4dc0561c17a0a23bfa53ba4a1
kammagik <kammagik@yahoo.com>
2018-05-07 12:55:12 -0700

    declared routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
