b749307e466bae4b5029a0198b5f19b4d2d0f092
Daniel Chamorro <dan@dcitllc.com>
2018-06-28 11:41:59 -0400

    DanChamorro - Completed DayI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">About</Link>
+        <br />
+        <Link to="/contact">Contact</Link>
       </div>
     </div>
   );
