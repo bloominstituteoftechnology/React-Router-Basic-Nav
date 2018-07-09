9243ec85161c0aa00719e065fa2b09d849130fa2
Your Name <you@example.com>
2018-07-09 14:53:03 -0700

    import fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
-import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact} />
+    <Route path="/contacts" component={Contact} />
   </div>
 );
 
