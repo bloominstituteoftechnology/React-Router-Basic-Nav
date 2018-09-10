c96234b3f084ce0d5edb9e8e2f95256bfbcea9e4
m-j-hacker <mjhacker@gmail.com>
2018-09-10 14:47:32 -0500

    Set exact path for Home Route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route path="/" component={Home} />
+    <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
