c90f9afe62de4bf5c11af1a82ab577b0e9c20089
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-10 15:12:53 -0400

    initial commit, imported BrowserRouter

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export { Home, About, Contact, Navigation };
