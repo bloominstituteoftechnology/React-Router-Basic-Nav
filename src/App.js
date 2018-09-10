daabbc5d3bfc47cef43b8a9715d060cf64884dad
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-10 15:08:48 -0400

    Implemented React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom";
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
   </div>
 );
 
