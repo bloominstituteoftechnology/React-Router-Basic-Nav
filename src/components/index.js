b9449f5e49cd0901ba1821f8ca903f719251dea7
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-10 15:20:35 -0400

    functionality correct, basic project complete

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 export { Home, About, Contact, Navigation };
