bdac3efd6f2ca7ef32fc5b1b43198a3f0ebba8b5
Timothy Hoang <timh1203@gmail.com>
2018-09-10 15:58:06 -0400

    Add navigation link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from 'react'
+import './App.css'
+import { Home, About, Contact, Navigation } from './components'
 import { Route } from 'react-router-dom'
 
 const App = () => (
@@ -11,6 +11,6 @@ const App = () => (
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
-);
+)
 
-export default App;
+export default App
