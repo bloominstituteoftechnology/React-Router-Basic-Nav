9989479c2a86e1742c826bf73581a30ce6bcdcc6
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-09-10 16:05:04 -0500

    nav set up and fixed minor error in declarations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
