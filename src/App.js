66495093bd894d140d203f4adceac2e314f483bd
Clint Kunz <clintkunz@gmail.com>
2018-09-10 14:31:25 -0600

    set up navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-    <Router exact path="/" component={Home} />
-    <Router path="/about" component={About} />
-    <Router path="/contact" component={Contact} />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
