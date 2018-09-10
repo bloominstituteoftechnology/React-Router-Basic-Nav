6558e193711058a27b91e740702917786aa710b1
iAmAdamReid <adam@adamreid.me>
2018-09-10 13:27:18 -0700

    Basic project complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
-
-const Router = BrowserRouter;
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 <Router>
