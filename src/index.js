814f3fe36ecb605af8174a6b50beb0addde86db5
iAmAdamReid <adam@adamreid.me>
2018-09-10 13:24:21 -0700

    Routes and Links

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Router = BrowserRouter;
+
+ReactDOM.render(
+<Router>
+<App />
+</Router>, document.getElementById('root'));
