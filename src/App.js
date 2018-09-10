8f0aef54707f40c03046a07b446ac8716cf57766
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-09-10 15:55:36 -0500

    wrapped App with Router comp, routes declared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
