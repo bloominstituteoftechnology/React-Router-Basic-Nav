62c9dbf1e36747114382a1bbcb330cbd611b8c60
Clint Kunz <clintkunz@gmail.com>
2018-09-10 14:28:03 -0600

    declared routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
+    <Router exact path="/" component={Home} />
+    <Router path="/about" component={About} />
+    <Router path="/contact" component={Contact} />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRoutet as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
