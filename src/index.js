cd1a055e6bd13d5e5afb2b43c49a018866347178
ezra davis davis <ezra.rr.davis@gmail.com>
2018-09-10 15:51:51 -0700

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import {ThemeProvider} from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    
+    <Router>
+        <App />
+    </Router>
+, 
+document.getElementById('root'));
+
+const color = {
+  mainColor: "#D9491D",
+  sideColorLight1: '#F4F4F4',
+  sideColorLight2: '#7F8489',
+  sideColorDark1: '#3D3D3B',
+  sideColorDark2: '#525F7F'
+}
+
+const theme = {
+  navTheme: `
+  color: ${color.sideColorLight1}
+  background: ${color.mainColor}
+  h1{
+    color:${color.sideColorDark1}
+  }
+
+  border-bottom: 2px solid ${color.sideColorDark2}
+  `
+} 
