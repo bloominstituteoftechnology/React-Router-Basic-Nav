6b9d3f300a09243436327bb6669f3c2365917710
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-10-06 23:25:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
