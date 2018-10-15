1b1dabf96468c7d3d316862cdd0a3f1494e846f7
Carlos Lantigua <clantigua954@gmail.com>
2018-10-15 16:25:33 -0400

    Carlos Lantigua

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+	<div>
+		<Navigation />
+		<Route exact path="/" component={Home} />
+		<Route exact path="/about" component={About} />
+		<Route exact path="/contact" component={Contact} />
+	</div>
 );
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="App">
+				<h1>React Router Mini</h1>
+				<div>
+					<Link to="/">Home</Link>
+				</div>
+				<div>
+					<Link to="/about">About</Link>
+				</div>
+				<div>
+					<Link to="/contact">Contact</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navigation;
