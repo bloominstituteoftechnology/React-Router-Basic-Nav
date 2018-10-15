f048d8267e9b5b4b7cc5dbf55441d6a2e7472508
Angelo Deleon <rudy209@comcast.net>
2018-10-15 14:00:44 -0700

    Finished MVP

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,7 @@ import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 export { Home, About, Contact, Navigation };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(<App />, document.getElementById('root'));
