ccba4cdb40af6ace6f60857c09b6c771e3c76542
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-15 16:25:45 -0400

    navigation.js done with <Link />

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const LinkWrapper = styled.div`
+    display : flex;
+    flex-direction : column;
+    font-size : 20px;
+    font-weight : bold;
+    color : #1ab188;
+`;
+
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
+
         <h1>React Router Mini</h1>
-        <div>
+        <LinkWrapper >
+            <Link to = "/">Home</Link>
+            <Link to = "/about">About</Link>
+            <Link to = "/contact">Contact</Link>
+        </LinkWrapper>
+
+        { /*<div>
           <a href="">Home</a>
         </div>
         <div>
@@ -13,7 +31,7 @@ const Navigation = () => {
         </div>
         <div>
           <a href="">Contact</a>
-        </div>
+        </div> */}
       </div>
     </div>
   );
