d7877d1d17110ad557f820a133c350e4768ba29d
samsisle <samko9522@gmail.com>
2018-10-15 15:51:18 -0700

    Finish MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
 );
 
