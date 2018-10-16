21da50341be0e4d6e6c03ca6e84222ff7e10d430
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-16 05:20:09 -0500

    react router navigation done plus some pink fonts :p

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+
+        <section className="nav-menu">
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </section>
       </div>
     </div>
   );
