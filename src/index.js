99d8dbea2ca3d62fa55c7d0b67b2cdf37a4147b3
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-17 15:38:28 -0400

    practiced basic router setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css"
+import "bootstrap/dist/css/bootstrap.css"
+import App from "./App"
+
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById("root")
+)
