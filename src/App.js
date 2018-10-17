98c713459b5c510827b3158832c335f6fcbe2a79
Joseph Thompson <joe.thompson838@gmail.com>
2018-10-16 21:01:39 -0400

    added 3 routes for home,contact,andabout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { Route } from 'react-router-dom';
+import { Route } from 'react-router';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path='/' exact component={Home}></Route>
+    <Route path='/about' component={About}></Route>
+    <Route path='/contact' component={Contact}></Route>
   </div>
 );
 
