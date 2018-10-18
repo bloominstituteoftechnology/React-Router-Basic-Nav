38643c7efbe92c33227f09a8e0473c43a3de9c49
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-10-17 21:53:29 -0500

    finished.

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,13 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export { Home, About, Contact, Navigation };
+ReactDOM.render(
+<Router>
+    <App />
+</Router>
+, document.getElementById('root')
+);
