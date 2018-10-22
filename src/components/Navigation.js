e3c26f04d3d5342e6491ef48458930d7a36f6d2d
jaklundun <govi.escudero@gmail.com>
2018-10-22 15:13:33 -0700

    Set up working react-router Links for the Navigation compnt.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <Link to='/'><div color="secondary">Home</div></Link>
+        <Link to='/about'><div color="secondary">About</div></Link>
+        <Link to='/contact'><div color="secondary">Contact</div></Link>
       </div>
     </div>
-  );
+  )
 };
 
 export default Navigation;
+
