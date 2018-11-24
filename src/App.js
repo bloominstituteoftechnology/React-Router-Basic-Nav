761cfc89e5bea76779f3b025908d3248f1d6b41f
Josh Z <jzieger10@gmail.com>
2018-11-24 13:52:00 -0500

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" />
+    <Route path="/about" />
+    <Route path="/contact" />
   </div>
 );
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
+
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+
 
 export { Home, About, Contact, Navigation };
