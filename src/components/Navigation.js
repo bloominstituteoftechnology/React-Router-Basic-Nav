d4fbbff3f91d462aaae25dc45281ee90eaaaa38c
Jasmine <jasmineanderson315@yahoo.com>
2018-11-26 16:55:56 -0500

    completed MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import '../App.css';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <h1>React Router Mini</h1>  
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/about">About</Link>
+        <Link className="nav-link" to="/contact">Contact</Link>     
       </div>
     </div>
   );
