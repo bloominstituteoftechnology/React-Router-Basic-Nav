b8af2521fae2e9346b727a785eee4d9e91431a17
Chaya Otikor <c.otikor@gmail.com>
2018-11-26 14:45:18 -0700

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      <Navigation />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact}  /> 
+      </div>
+    )
+  }
+};
 
 export default App;
