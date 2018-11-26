44862e0926d69499456c663b7e5a30b89cf5d073
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-26 13:13:28 -0600

    add Routes to home, about, and contact to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+// Importing libraris
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+// Importing styles
 import './App.css';
+
+// Importing components
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
