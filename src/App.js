f2c387a61b737ad255fc56eab6f72b34609bf0c5
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-26 12:55:49 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const App = () => (
   <div>
