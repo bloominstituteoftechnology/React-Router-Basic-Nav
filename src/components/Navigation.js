767709ea2e6c5dff70c28288d54417d7d8e4c3de
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-11-26 12:22:58 -0800

    Add all links to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom'
+
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
+          <Link to="/">
           <a href="">Home</a>
+          </Link>
         </div>
         <div>
+          <Link to="/about">
           <a href="">About</a>
+          </Link>
         </div>
         <div>
+          <Link to="/contact">
           <a href="">Contact</a>
+          </Link>
         </div>
       </div>
     </div>
