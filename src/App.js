4c27d07108ce83a508dd28defa83507c7fdb57c4
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-26 16:52:05 -0500

    "App.js routing completed"

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Home, About, Contact, Navigation } from './components';
 import { Route } from 'react-router-dom';
 
 
+
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/"/>
-    <Route />
-    <Route />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
 
   </div>
 );
