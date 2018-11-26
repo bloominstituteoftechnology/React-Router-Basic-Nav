1d6214f9d2fc840b163a62dbcbe455e1b5e4305e
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-26 16:35:24 -0500

    "app.js route added"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
+
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/"/>
+    <Route />
+    <Route />
+
   </div>
 );
 
