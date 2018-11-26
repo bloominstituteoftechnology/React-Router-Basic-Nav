b05864d98fa3f92bae753161f5eb60f787b56cfb
turkworks <cengizbug@Cengizs-iMac.local>
2018-11-26 09:59:37 -0700

    finished basic-nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
   </div>
-);
+    )
+  }
+  
+};
 
 export default App;
