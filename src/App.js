216f6a9f8987ca513538b73fb43c5ac8c9618cfb
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-12-04 17:54:13 -0500

    navigation.js renders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path ="/" component ={()=> <div>Home</div>} />
+    <Route path ="/About" component ={()=> <div>About </div>} />
+    <Route path ="/Contact" component ={()=> <div> Contact</div>} />
   </div>
 );
 
 export default App;
+
+//Navigation and Route in App are siblings
