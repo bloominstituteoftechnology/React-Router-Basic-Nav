466c53a606c8d5eaf9aac5fed2ae24acbb679c35
Noah <noah.stoeckel@gmail.com>
2019-01-14 16:38:12 -0500

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
   </div>
 );
 
