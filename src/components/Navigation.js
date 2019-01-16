b401a92a3d2da31240c94fbce8cc30385b3625c2
brettmadrid <brettmadrid9@gmail.com>
2019-01-16 10:22:46 -0800

    Completed MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,15 +8,12 @@ const Navigation = () => {
         <h1>React Router Mini</h1>
         <div>
           <Link to={'/'}>Home</Link>
-          {/* <a href="">Home</a> */}
         </div>
         <div>
           <Link to={'/about'}>About</Link>
-          {/* <a href="">About</a> */}
         </div>
         <div>
           <Link to={'/contact'}>Contact</Link>
-          {/* <a href="">Contact</a> */}
         </div>
       </div>
     </div>
