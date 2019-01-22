46f3a36e523e38fc15c147e68e9cfb589c9993f3
Randy Wilson <phoenixrr2113@gmail.com>
2019-01-21 19:53:36 -0500

    finished mvp. added all routes and app is rendering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
 	<div>
-		<Route path="/" component={Home} />
+		<Route exact path="/" component={Home} />
 		<Route path="/about" component={About} />
 		<Route path="/contact" component={Contact} />
 		<Navigation />
