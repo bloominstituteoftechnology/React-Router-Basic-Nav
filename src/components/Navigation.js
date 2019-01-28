3883faa80158c2aede95a4ebd7d120e1c49e3169
J A <jarata@gmail.com>
2019-01-28 10:48:42 -0800

    reset

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link to="/">Home</Link>
+          <a href="">Home</a>
         </div>
         <div>
-          <Link to="/about">About</Link>
+          <a href="">About</a>
         </div>
         <div>
-          <Link to="/contact">Contact</Link>
+          <a href="">Contact</a>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
