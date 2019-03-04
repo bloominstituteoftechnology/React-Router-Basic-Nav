a35dbc8063ec9877ccb8544f955f83666c511061
jkaddu <jkaddu@yahoo.com>
2019-03-04 13:08:19 -0800

    done with set up g

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" exact component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+document.getElementById('root'));
