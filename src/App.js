21b698564ab1d6998050667678ed3e8110020b33
Zero <jrr043@latech.edu>
2019-03-27 11:59:00 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path='/' exact component={ Home } />
+    <Route path='/contact' component={ Contact } />
+    <Route path='/about' component={ About } />
   </div>
 );
 
