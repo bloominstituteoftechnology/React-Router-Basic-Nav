affea76a7a2b636f81e658c96983698e6b2a9556
Scott Grobe <gitgrobe@gmail.com>
2019-03-26 23:55:53 -0400

    initial commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
