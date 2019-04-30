564450317e830b3e20c2a3c7468da608d3ce8db6
mag16 <marcoguzman16@gmail.com>
2019-04-29 21:03:34 -0700

    react router mini practice completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
-
+import Navigation from './components/Navigation';
+import { Home, About, Contact} from './components';
 import { Route } from 'react-router-dom';
 
 const App = () => (
@@ -10,8 +10,6 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
-
   </div>
 );
-
 export default App;
