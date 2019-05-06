74f5c82ace30cf82fc4389a85f4b35ba4a48b994
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 13:19:40 -0700

    add Router and wrap top level App component in it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './App';
+
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+     document.getElementById('root')
+);
