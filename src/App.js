4ee71d84d7bb39378e793a3754f159ba0e9a667c
Manju KC <Manju KC>
2019-05-06 15:05:05 -0400

    finishedfirst assignent for today

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" compounent={Home}/>
+    <Route  exact  path="/About" compounent={About}/>
+    <Route path="/Contact" compounent={Contact}/>
   </div>
 );
 
