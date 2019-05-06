a00d22b3b8af75006c86dfb07f8ba6935951e149
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 13:19:57 -0700

    add all individual Routes inside App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
+
 import { Home, About, Contact, Navigation } from './components';
+
+import './App.css';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home}></Route>
+    <Route path='/about' component={About}></Route>
+    <Route path='/contact' component={Contact}></Route>
   </div>
 );
 
