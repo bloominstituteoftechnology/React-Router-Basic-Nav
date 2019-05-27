3b43862bd1b9e1f4043b6c34ea42117883b71382
Cory Hedeen <hedeencory@gmail.com>
2019-05-27 06:51:23 -0400

    set Route paths

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
     <Navigation />
+
   </div>
 );
 
