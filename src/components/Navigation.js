361a2d4262f347e9d04b0fb2eadb7aa74d9a3b25
maxdavid <me@maxdavid.com>
2019-06-10 12:41:35 -0700

    Add MVP with simple routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
+      <div className='App'>
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to='/'>Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to='/about'>About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to='/contact'>Contact</Link>
         </div>
       </div>
     </div>
