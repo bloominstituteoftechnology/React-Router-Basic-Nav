4a5db154d37ce448aabff9ad77b12b76b3b8b703
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-06-19 19:05:28 -0400

    Corrected some syntax where I included exact on all the components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    
+
     <Route path="/" exact component={Home} />
-    <Route path="/about" exact component={About} />
-    <Route path="/contact" exact component={Contact} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
