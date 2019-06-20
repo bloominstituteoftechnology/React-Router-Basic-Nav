999535336fc147abb74a5821c93e504c61132421
Ronald Cho <ronaldcho@gmail.com>
2019-06-20 10:10:33 -0700

    Navigation.js with Link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div className="nav-item">
-      <div>
-        <
-        Navigation />
-      </div>
+        <Navigation />
         <div>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/contac" component={Contact} />
+          <Route path="/contact" component={Contact} />
         </div>
   </div>
 );
