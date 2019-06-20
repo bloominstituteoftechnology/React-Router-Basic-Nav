3e0db2b5172cf7f544a39fc161d91dd74435a2c4
Travis Little <kingtrav@gmail.com>
2019-06-19 18:00:29 -0600

    mvp met - i think?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
+    <Route path="/" exact component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
     <Navigation />
   </div>
 );
