1e0e5ad6464f76c9f4fee3d0bf3e59ca5e105302
dangolbeeker <scottb305@outlook.com>
2019-07-22 15:59:44 -0400

    added route paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
+  <>
   <div>
     <Navigation />
   </div>
+  <Route path="/" exact component ={Home}/>
+  <Route path="/about" component ={About}/>
+  <Route path="/contact" component ={Contact}/>
+ </>
 );
 
 export default App;
