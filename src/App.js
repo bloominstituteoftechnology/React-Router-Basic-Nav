3651a01b99e6b30d72a64b9556cf3b7b61fd65ea
Mark French <markedfrench@gmail.com>
2019-07-22 15:38:14 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Switch, Link } from "react-router-dom";
+
+import './App.css';
+
+
 
 const App = () => (
-  <div>
+  <Router>
     <Navigation />
-  </div>
+    <Link to="/">Home{' '}</Link> 
+     <Link to="/about">About{' '}</Link> 
+     <Link to="/contact">Contact</Link>
+     
+     <Switch>
+          <Route path="/" exact component={Home} />
+         
+          <Route path="/about/" component={About} />
+          <Route path="/contact/" component={Contact} />
+      
+          
+        </Switch>
+  
+  </Router>
 );
 
 export default App;
