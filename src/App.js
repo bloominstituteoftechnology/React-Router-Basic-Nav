13cc9608aa95b56193a62efc5da2e949bea6757c
spettigrew <sara.pettigrew5@gmail.com>
2019-09-04 11:25:44 -0600

    Notes added to js files

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import { Home, About, Contact, Navigation } from './components';
 const App = () => (
   <div>
     <Navigation />
-    <Home />
-    <About />
-    <Contact />
-
-    <Route path="/navigation" component={Navigation} />
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact} />
-
+    <Route exact path="/" component={Home}      />
+    <Route path="/about/" component={About}      />
+    <Route path="/contact" component={Contact}  />
   </div>
 );
 
 export default App;
+
+// 'exact' is the same as {true}. It is a named prop. 'Exact' is the exact path. Won't go past "home".
+// <Switch> turns your routes from 'if statements' into if/else. Switch only matches one component. '/' Will not look further than the '/' for something '/about', etc. Must import Switch with Route from react-router-dom.
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const About = () => <h1>About</h1>;
+const About = (props) => {
+    console.log('About', props);
+return (
+ <h1>
+     About 
+    {/*{props.match.params.id} */}
+    {/* <button onClick={() => {
+        console.log('Button is Clicked');
+        props.history.push('/');
+    }}>Navigate to Home</button> */}
+ </h1>
+  );
+};
 
 export default About;
+
+// have about/:id in App.js for props.match, etc to work.
+
+// <button OnClick={() => {
+//     console.log('We Clicked Back');
+//     props.history.goBack();
+// }}>Go Back</button>
