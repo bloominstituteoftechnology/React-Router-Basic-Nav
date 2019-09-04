210b5cca981bd8aeba883e397be24a737aa70bae
spettigrew <sara.pettigrew5@gmail.com>
2019-09-03 19:56:56 -0600

    changes to JS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Home />
+    <About />
+    <Contact />
+
+    <Route path="/navigation" component={Navigation} />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+
   </div>
 );
 
