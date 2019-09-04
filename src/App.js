5ebbb7a3b707874f0c099f956998a41bf091648b
Greg Renfro <remph819@gmail.com>
2019-09-04 04:30:37 -0700

    added browserrouter and route to folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+	<div>
+		<Navigation />
+	</div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
