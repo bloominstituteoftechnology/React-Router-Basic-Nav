1b21155b1162421c44c369295fa7e88d3a3f549b
Steve Renner <che1111@hotmail.com>
2019-09-16 13:39:18 -0700

    Cotent in correct index.js file, reached MVP

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+
+
+
+
 
 export { Home, About, Contact, Navigation };
