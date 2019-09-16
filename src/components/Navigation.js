db589a85b78d086667301ea5ec4fd35229f740b8
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-16 12:20:11 +0100

    replaced the anchor tag with Link to=/about >About </Link>

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,10 +8,9 @@ const Navigation = () => {
         <h1>React Router Mini</h1>
         <div>
           <Link to="/" >Home</Link>
-          <a href="">Home</a>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about" >About </Link>
         </div>
         <div>
           <a href="">Contact</a>
