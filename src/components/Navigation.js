d3dd370faebbcef083d8fe9ffa7ed4f34262c0d1
seanaleid <seanaleid@gmail.com>
2019-09-16 16:12:29 -0400

    MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link to='/'>Home</Link>
+          <Link to='/'>Home!</Link>
         </div>
         <div>
           <Link to='/about'>About</Link>
