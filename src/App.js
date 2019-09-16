fe71ff71ebc1721b3c5ec9ed01ba53278375f529
AbdelIdir <homeworkoracle@protonmail.com>
2019-09-16 14:47:20 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'; ;
+
+
 
 const App = () => (
   <div>
+   <Router>
     <Navigation />
-  </div>
+
+    <Route exact path = "/home" component ={Home} />
+    <Route path = "/about" component ={About} />
+    <Route path = "/contact" component ={Contact} />
+    </Router>
+  </div> 
+     
+    
+
+
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+  } from 'react-router-dom'; ;
 ReactDOM.render(<App />, document.getElementById('root'));
