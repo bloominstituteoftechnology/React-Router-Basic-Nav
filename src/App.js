7c49a0d707178b83be64c5439777cd04e23a15ea
anatulea <anatulea14@.gmali.com>
2019-10-03 18:05:07 -0500

    inicial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route } from 'react-router-dom';
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about"components={About}/>
+      <Route path ="/contact"  components={Contact}/>  
+    
   </div>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+    <div className="App">
+      <h1>React Router Mini</h1>
+      <nav>
+      <div>
+        <Link to="/">Home</Link>
       </div>
-    </div>
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
+      </nav>
+       
+      </div>
+  </div>
   );
 };
 
