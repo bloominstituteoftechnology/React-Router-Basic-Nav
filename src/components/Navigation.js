c256946e83583256b6d8f49c637991ff78d3d97a
Ricardo Barbosa <ric3639@gmail.com>
2019-10-05 09:08:25 -0700

    Initial Commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
-        </div>
+        <Link to="/">About</Link>       
+         </div>
         <div>
-          <a href="">Contact</a>
+        <Link to="/">Contact</Link>        
         </div>
       </div>
     </div>
