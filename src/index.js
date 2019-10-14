e6c1b0b62fdf8c985de0af59affd6d06760ef5e3
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-14 14:25:07 -0500

    Route and Link added to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 ReactDOM.render(
 <Router>
