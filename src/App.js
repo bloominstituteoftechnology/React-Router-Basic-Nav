c2fbac5d6904aa9bb58cab373ba7872c516da5aa
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-14 14:10:53 -0500

    routes for navigation declared and linked

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import {Route} from "react-router-dom";
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path = "/" component={Home}/>
+    <Route path = "/About" component={About}/>
+    <Route path = "/Contact" component={Contact}/>
+  
   </div>
 );
 
