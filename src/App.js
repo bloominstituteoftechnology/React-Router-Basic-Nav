e35ced462ae74e8623cde3de05ffb2c9f6f370e6
Michael Lowe <mrlowememphis@yahoo.com>
2019-10-29 21:12:23 -0500

    MVP met Correct import tags, Route paths and Links added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
+import {BrowserRouter, Router} from 'react-router-dom';
 
 const App = () => (
+
+  <>
   <div>
     <Navigation />
-  </div>
+    </div>
+
+    <Route exact path="/" component={Home} />
+    <Route path="/About" component={About} />
+    <Route path="/Contact" component={Contact} />
+ 
+  </>
 );
 
 export default App;
+
+
+
+
+
+
+
+// import React from 'react';
+// import './App.css';
+// import { Home, About, Contact, Navigation } from './components';
+
+// const App = () => (
+//   <div>
+//     <Navigation />
+//   </div>
+// );
+
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
-
-export { Home, About, Contact, Navigation };
+ import Home from './Home';
+ import About from './About';
+ import Contact from './Contact';
+ import Navigation from './Navigation';
+ export { Home, About, Contact, Navigation };
