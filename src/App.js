270ecf47512dab97baf1f4dc2a7fb94d4520b8f1
leifgerchberg <leifgerchberg@gmail.com>
2019-11-11 14:17:30 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom'
+
+import Home from './components/Home'; 
+import Contact from './components/Contact'; 
+import About from './components/About'; 
+import Navigation from './components/Navigation'; 
+
 
 const App = () => (
   <div>
-    <Navigation />
+      <Navigation />
+      <Route path="/" exact component={Home} />
+      <Route path="/about/" component={About} />
+      <Route path="/contact/" component={Contact} />
   </div>
 );
 
