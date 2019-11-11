ac9181735fbe07928b4376e3a7982050a13d5a88
blakelower <blakelower1999@gmail.com>
2019-11-11 20:49:33 +0700

    added the link to the page

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
