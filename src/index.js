354c25cb8e51be5ce381fc73ac8d8b513dcbaf6d
jarrodskahill <jarrodskahill@gmail.com>
2019-11-11 14:58:53 -0800

    finished MVP

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router} from "react-router-dom"
 
-ReactDOM.render(<Route><App /></Route>, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
