5233d059fbfbe2a563a0c424b1af7bd44583610a
miriam-sullivan <maljayd42@gmail.com>
2019-11-12 15:43:57 -0600

    added more of step 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Route } from "react-router-dom";
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
