ad0641bdbcb3f623da09a9d50ab61d5737a1ae49
occludedarp <cmrnmr@gmail.com>
2019-11-12 10:54:19 -0500

    added routes and links to about contact and home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { Home, About, Contact, Navigation } from './components';
 const App = () => (
   <div>
     <Navigation />
+    
+    <Route exact path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/contact" component={Contact}/>
+    
   </div>
 );
 
