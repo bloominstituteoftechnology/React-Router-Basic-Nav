12d03366693accfc85ca7a154617e9b7af7693e0
Nicole white <you@example.com>
2019-12-15 19:23:57 -1000

    route and links in nav set and working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+    <BrowserRouter>
+    <Route exact path = "/" component = {Home}/>
+    <Route path = "/about" component = {About}/>
+    <Route path = "/contact" component = {Contact}/>
     <Navigation />
+    </BrowserRouter>
   </div>
 );
 
