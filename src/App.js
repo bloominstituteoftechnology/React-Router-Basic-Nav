363a3f7fddd3b197c7e60a84d2dcf5b9a336648a
Anthony Streett <arjstreett@gmail.com>
2019-12-17 11:40:26 -0500

    MVP COMPLETE!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+     <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
     <Navigation />
+    </div>
   </div>
 );
 
