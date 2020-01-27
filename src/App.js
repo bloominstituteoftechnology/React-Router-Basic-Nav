fb8306eb26f42bc12acdfb215efadce667093b55
alexis villaraza <alexis.borja@gmail.com>
2020-01-27 13:38:22 -0700

    added links as necessary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Link, Switch, Route} from 'react-router-dom';
 
-const App = () => (
+const App = () => {
+  return (
   <div>
     <Navigation />
-  </div>
-);
+    
+
+<Switch>
+<Route exact path='/' component={Home}/>
+
+
+  <Route exact path='/about'>
+    <About />
+  </Route>
+
+  <Route exact path='/contact'>
+    <Contact/>
+  </Route>
+ </Switch>
+ 
+ </div>
+  );
+ };
+ 
 
 export default App;
