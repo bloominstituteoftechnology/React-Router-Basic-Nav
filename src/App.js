5981a103895eedb9b849a7fdac01a262e94ee0bf
Mandi Haase <haase1020@gmail.com>
2020-01-27 16:48:44 -0500

    MVP again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import Bob from './components/Bob';
+import { Route } from 'react-router-dom';
+
+
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home}/>
+    <Route path="/contact" component={Contact}/>
+    <Route path="/about" component={About}/>
+    <Route path="/fun" component={Bob}/>
   </div>
 );
 
