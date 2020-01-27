d54d7cd73169e4c8d4c2d547393236ef8f5f89ff
alexis villaraza <alexis.borja@gmail.com>
2020-01-27 13:26:57 -0700

    imported appropriate items to each .js page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {link} from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,16 +7,18 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
     </div>
+
+   
   );
 };
 
