7600e1a74bf6536bc474711cd28210ab642cc18c
blakelower <blakelower1999@gmail.com>
2020-01-27 22:44:26 +0700

    made it

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export { Home, About, Contact, Navigation };
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
