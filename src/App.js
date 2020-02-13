5cd00b0bb07af78f4f54c880a3ba9711b8e98f5f
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-12 22:46:49 -0500

    set up app & nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
-    <Navigation />
+  <div >
+  <Navigation />
+  <Route path="/" exact component={Home} />
+  <Route path="/about" exact component={About} />
+  <Route path="/contact" exact component={Contact} />
   </div>
 );
 
