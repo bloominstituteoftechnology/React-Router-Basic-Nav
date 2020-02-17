b9f35a55dab0e9cb594acca0f6ef60c8d26857a1
Jason Neale <jason@novus.one>
2020-02-16 20:54:16 -0500

    fixed typo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+            <div><Link to="/">Home</Link></div>
+            <div><Link to="/about">About</Link></div>
+            <div><Link to="/contact">Contact</Link></div>
       </div>
     </div>
   );
