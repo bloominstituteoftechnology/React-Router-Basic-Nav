e139b6eae87b46a3299e136249cfbc10172daac6
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-24 16:52:11 -0500

    completed the Navigation transformation to use <Link/>

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import {Route} from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route />
-    <Route />
-    <Route />
+    {/* Create 3 <Route /> components and set the path equal to
+        / , /about, /contact respectively
+    */}
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/" component={About}/>
+    <Route exact path="/" component={Contact}/>
   </div>
 );
 
