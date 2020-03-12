dbac69a29d05711b5ac0cc7136a1eefb1de42392
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-03-12 16:16:27 -0700

    added routing to navigation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter as Router} from 'react-router-dom';
+ReactDOM.render(<Router> <App /> </Router>, document.getElementById('root'));
