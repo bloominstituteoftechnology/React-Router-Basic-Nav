1e618d5a74af830fe25ece73725a897e67844bce
Jose Sustaita <josesustaita70@gmail.com>
2020-03-15 19:06:02 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+import { Route, Redirect } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/about">
+      <About />
+    </Route>
+    <Route exact path="/contact">
+      <Contact />
+    </Route>
+    <Redirect to="/" />
   </div>
 );
 
